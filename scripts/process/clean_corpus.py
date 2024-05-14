0dcf8968ec608a95a8ee0e93105123d5c94fed41
Marie Delporte <marie.delportelandat@gmail.com>
2024-05-14 11:16:38 +0200

    feat: ajout script nettoyage corpus

---
+++ scripts/process/clean_corpus.py
@@ -0,0 +1,56 @@
+import os
+import re
+
+dossier = '../data/clean/'
+
+def nettoyer_fichier(chemin):
+    with open(chemin, 'r') as f:
+        texte = f.read()
+
+        # Supprimer le contenu entre "Figures Citation" ou "Citation" et "Introduction"
+        pattern = r'Figures Citation|Citation'
+        match = re.search(pattern, texte)
+        debut_suppression = match.start()
+        fin_suppression = texte.find('Introduction', debut_suppression)
+        texte = texte[:debut_suppression] + texte[fin_suppression:]
+
+        # Supprimer le contenu à partir de "References" jusqu'à la fin
+        debut_suppression = texte.find('References')
+        texte = texte[:debut_suppression]
+
+        # Supprimer les liens
+        texte = re.sub(r'https?://\S+', '', texte)
+
+        # Supprimer les schémas de type [ 1 – 3 ], [ 1 ], [ 10 ], [ 2 , 3 ], [ 1 ]
+        texte = re.sub(r'\[(\s?\d+\s?–)+\]', '', texte)
+        texte = re.sub(r'\[\d+\]', '', texte)
+        texte = re.sub(r'\[(\s?\d+\s?,)+\]', '', texte)
+        texte = re.sub(r'\[\s?\d+\s?\]', '', texte)
+
+        # Supprimer les schémas de type ( Fig 5B and 5C )
+        texte = re.sub(r'\(.*?Fig.*?\)', '', texte)
+
+        # Supprimer les titres des sections : Abstract Introduction Results
+        texte = re.sub(r'Abstract ', '', texte)
+        texte = re.sub(r'Introduction ', '', texte)
+        texte = re.sub(r'Results ', '', texte)
+
+
+        # Écrire le texte nettoyé dans le même fichier
+        with open(chemin, 'w') as f:
+            f.write(texte)
+
+# Parcourir récursivement tous les fichiers .txt à partir du dossier principal
+fichiers_trouves = False
+for dossier, sous_dossiers, fichiers in os.walk(dossier):
+    for nom_fichier in fichiers:
+        if nom_fichier.endswith('.txt'):
+            chemin_complet = os.path.join(dossier, nom_fichier)
+            nettoyer_fichier(chemin_complet)
+            print(f"Nettoyage effectué pour {chemin_complet}")
+            fichiers_trouves = True
+
+if not fichiers_trouves:
+    print(f"Aucun fichier .txt n'a été trouvé dans {dossier} ou ses sous-dossiers")
+else:
+    print("Nettoyage terminé pour tous les fichiers .txt")
